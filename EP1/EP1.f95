24e9c7cb8f136edaa06116df4f8bfd7564dbac4d
Lucas Magno <lmagno94@gmail.com>
2015-08-25 15:09:42 -0300

    Utiliza precisão dupla como padrão

--- EP1/EP1.f95
+++ EP1/EP1.f95
@@ -3,13 +3,14 @@ use entrada
 use cholesky
 implicit none
     integer :: n, i, j, k
-    real*8,  allocatable :: A(:, :), b(:)
+    real,  allocatable :: A(:, :), b(:)
 
 
     call le_sistema(n, A, b)
     k = cholcol(n, A)
     print *, k
-    
+    print *, kind(1.0)
+
     ! Desaloca a matriz A e o vetor b
     deallocate(b)
     deallocate(A)
