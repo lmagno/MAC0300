2f721c212914387bb617216a086daf7050066d58
Lucas Magno <lmagno94@gmail.com>
2015-08-27 15:33:01 -0300

    Algumas mudanças

--- EP1/cholesky.f95
+++ EP1/cholesky.f95
@@ -2,12 +2,40 @@ module cholesky
 implicit none
 
 contains
-  function cholcol(n, A)
+  ! Calcula o fator de Cholesky pelo método do produto externo,
+  ! orientado para colunas.
+  ! Guarda o fator na parte triangular inferior da matriz A e
+  ! retorna:
+  !     0: se o fator foi calculado com sucesso
+  !    -1: se a matriz A não é positiva definida e não foi
+  !        possível calcular seu fator
+  function cholcol(n, A) result(status)
     integer, intent(in) :: n
     real, intent(inout) :: A(:, :)
-    integer :: cholcol
+    real :: akk
+    integer :: status
+    integer :: i, j, k
 
-    cholcol = n
+    status = -1
+    do k = 1, n
+       akk = A(k, k)
+       if (akk <= 0) then
+          return
+       end if
+
+       akk = sqrt(akk)
+       A(k, k) = akk 
+       do i = k+1, n
+          A(i, k) = A(i, k)/akk
+       end do
+       
+       do j = k+1, n
+          do i = k+1, n
+             A(i, j) = A(i, j) - A(i, k)*A(j, k)
+          end do
+       end do       
+    end do
+
+    status = 0
   end function cholcol
-
 end module cholesky
