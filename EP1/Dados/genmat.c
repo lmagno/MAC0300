f4b5b9d75baeef653c65fbe859084dfca3ec2769
Lucas Magno <lmagno94@gmail.com>
2015-09-09 20:18:39 -0300

    Gera as matrizes

--- EP1/Dados/genmat.c
+++ EP1/Dados/genmat.c
@@ -74,9 +74,13 @@ int main()
 
   fprintf(fp,"%d",n);
 
-  for (i=0; i<n; i++)
-    for (j=0; j<n; j++)
-      fprintf(fp,"\n%3d %3d % .20e",i,j,M[i][j]);
+    for (i=0; i<n; i++)
+     if (i==n-1)
+       for (j=0; j<n; j++)
+         fprintf(fp,"\n%3d %3d % .20e",i,j,M[0][j]);
+     else
+       for (j=0; j<n; j++)
+         fprintf(fp,"\n%3d %3d % .20e",i,j,M[i][j]);
 
   for (i=0; i<n; i++)
       fprintf(fp,"\n%3d % .20e",i,b[i]);
