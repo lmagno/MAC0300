97fa62820f07905b91305e5b267909e5e99c0481
Lucas Magno <lmagno94@gmail.com>
2015-09-11 10:10:57 -0300

    Commit emergencial pra não perder as coisas

--- EP1/Módulos/sol_lu.f95
+++ EP1/Módulos/sol_lu.f95
@@ -4,62 +4,21 @@ use   utils, only: pmatriz, pvec, swap, Results
 use entrada, only: le_sistema
 implicit none
 contains
-
-    ! Carrega A e b do sistema
-    !     Ax = b
-    ! com
-    !     A ∈ ℝⁿˣⁿ
-    !     b ∈ ℝⁿ
-    ! definidos no arquivo filename, resolve
-    ! o sistema por decomposição LU 
-    ! com orientação a colunas se possível
-    ! e guarda os tempos de execução e o erro do 
-    ! resultado em res.
-    ! Retorna:
-    !     0: caso a matriz não seja singular 
-    !        e o sistema foi resolvido com sucesso.
-    !    -1: caso contrário.
     function sol_lu_col(filename, res) result(status)
         character(len=*), intent(in)  :: filename
-          type (Results), intent(out) :: res
-           real, allocatable :: A(:, :)
-           real, allocatable :: x(:), b(:)
-        integer, allocatable :: p(:)
-        integer :: i, n, status
-           real :: start, finish
+          type (Results), intent(inout) :: res
+        integer :: status
 
-        call le_sistema(n, A, b, filename)
-        allocate(p(n))
-
-        call cpu_time(start)
-        status = lucol(n, A, p)
-        call cpu_time(finish)
-        res%tdecomp = finish - start
-
-        
-        if (status == -1) then
-           return
-        end if
-
-        status = sscol(n, A, p, b, res)
-        if (status == -1) then
-           return
-        end if
-
-        ! Aloca e calcula o vetor solução esperado
-        allocate(x(n))
-        x = [(1 + mod(i-1, n/100), i = 1, n)]
-
-        ! Calcula a norma do vetor diferença
-        ! entre a solução esperada e a obtida,
-        ! como forma de estimativa do erro
-        res%erro = norm2(x - b)/sqrt(real(n))
-
-        deallocate(A)
-        deallocate(b)
-        deallocate(x)
-        deallocate(p)
+        status = sol_lu_generic(filename, lucol, sscol, res)
     end function sol_lu_col
+
+    function sol_lu_row(filename, res) result(status)
+        character(len=*), intent(in)  :: filename
+          type (Results), intent(inout) :: res
+        integer :: status
+
+        status = sol_lu_generic(filename, lurow, ssrow, res)
+    end function sol_lu_row
 
     ! Carrega A e b do sistema
     !     Ax = b
@@ -75,9 +34,10 @@ contains
     !     0: caso a matriz não seja singular 
     !        e o sistema foi resolvido com sucesso.
     !    -1: caso contrário.
-    function sol_lu_row(filename, res) result(status)
+    function sol_lu_generic(filename, lu, ss, res) result(status)
         character(len=*), intent(in)  :: filename
-          type (Results), intent(out) :: res
+          type (Results), intent(inout) :: res
+          integer :: lu, ss
            real, allocatable :: A(:, :)
            real, allocatable :: x(:), b(:)
         integer, allocatable :: p(:)
@@ -88,14 +48,14 @@ contains
         allocate(p(n))
 
         call cpu_time(start)
-        status = lurow(n, A, p)
+        status = lu(n, A, p)
         call cpu_time(finish)
         res%tdecomp = finish - start
         if (status == -1) then
            return
         end if
 
-        status = ssrow(n, A, p, b, res)
+        status = ss(n, A, p, b, res)
         if (status == -1) then
            return
         end if
@@ -113,5 +73,5 @@ contains
         deallocate(b)
         deallocate(x)
         deallocate(p)
-    end function sol_lu_row
+    end function sol_lu_generic
 end module sol_lu
