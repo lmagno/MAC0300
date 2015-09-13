7e1c85fb091e2ca24ca3bf80d4b5f0395887fb04
Lucas Magno <lmagno94@gmail.com>
2015-09-13 15:13:45 -0300

    Define externamente os arquivos de entrada padrão

--- EP1/EP1.f95
+++ EP1/EP1.f95
@@ -5,42 +5,50 @@ program EP1
     implicit none
 
     integer                        :: i, n
-    character(len=72), allocatable :: filenames(:)
+    character(len=72)              :: filename
+    character(len=72), allocatable :: chol_filenames(:), lu_filenames(:)
 
     n = IARGC()
 
     ! Usa arquivos padrões caso nenhum seja passado
     ! como argumento
     if (n == 0) then
-       n = 9
-       allocate(filenames(n))
+        n = 9
+        allocate(chol_filenames(n))
+        allocate(  lu_filenames(n))
 
-       ! Arquivos de entrada padrões
-       filenames = ['Dados/a1.dat', &
-                    'Dados/a2.dat', &
-                    'Dados/a3.dat', &
-                    'Dados/a4.dat', &
-                    'Dados/a5.dat', &
-                    'Dados/a6.dat', &
-                    'Dados/a7.dat', &
-                    'Dados/a8.dat', &
-                    'Dados/a9.dat']
+        open(1, file = "cholesky_defaults", status = "old")
+        open(2, file =       "lu_defaults", status = "old")
+
+        ! Arquivos de entrada padrões
+        do i = 1, n
+            read (1, '(a)'), chol_filenames(i)
+            read (2, '(a)'),   lu_filenames(i)
+        end do
+
+        close(1)
+        close(2)
     else
-       ! Arquivos passados como argumentos
-       allocate(filenames(n))
-       do i = 1, n
-          call get_command_argument(i, filenames(i))
-       end do
+        ! Arquivos passados como argumentos
+        allocate(chol_filenames(n))
+        allocate(  lu_filenames(n))
+
+        do i = 1, n
+            call get_command_argument(i, filename)
+            chol_filenames(i) = filename
+              lu_filenames(i) = filename
+        end do
     end if
 
     print *, "=============================================== Cholesky ==============================================="
-    call solve_systems(filenames, sol_chol_col, sol_chol_row, "A matriz não é definida positiva!")
+    call solve_systems(chol_filenames, sol_chol_col, sol_chol_row, "A matriz não é definida positiva!")
 
     print *, ""
     print *, "================================================== LU =================================================="
-    call solve_systems(filenames, sol_lu_col, sol_lu_row, "A matriz é singular!")
+    call solve_systems(lu_filenames, sol_lu_col, sol_lu_row, "A matriz é singular!")
 
-    deallocate(filenames)
+    deallocate(chol_filenames)
+    deallocate(  lu_filenames)
 
 contains
   subroutine solve_systems(filenames, sol_col, sol_row, errmsg)
