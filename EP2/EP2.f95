4bd5d107ee5ffe8b02471f1a839a69855f92ecd3
Lucas Magno <lmagno94@gmail.com>
2015-11-02 11:16:35 -0200

    Implementa o método de Gradientes Conjugados

--- EP2/EP2.f95
+++ EP2/EP2.f95
@@ -2,61 +2,26 @@ program EP2
     use class_SparseMatrixCSC
     use class_SparseMatrixCOO
     use RandomSparseMatrix
+    use CG
     implicit none
 
     type(SparseMatrixCSC) :: A
     type(SparseMatrixCOO) :: tmp
     integer :: hasindex, i, j
     real :: v, w
-    ! real :: B(7), C(5)
-    !
-    ! call tmp%allocate(m = 4, n = 4, nnz = 4)
-    ! tmp%colind = [1, 2, 2, 3]
-    ! tmp%rowind = [2, 2, 4, 3]
-    ! tmp%val    = [5, 8, 6, 3]
-    !
-    ! call tmp%print
-    !
-    ! A = tmp%to_csc()
-    ! call A%print
-    !
-    ! call A%deallocate
-    ! call tmp%deallocate
+    real :: b(2), x(2)
 
-    ! call tmp%allocate(m = 5, n = 7, nnz = 9, &
-    !                   colind = [1,   2,  2,  3,  3,  4,  5,  6,  7], &
-    !                   rowind = [1,   1,  2,  2,  3,  3,  3,  4,  5], &
-    !                   val    = real([11, 22, 33, 44, 55, 66, 77, 88, 99]))
+    call tmp%init(2, 2)
+    hasindex = tmp%setindex(1, 1, 4.0)
+    hasindex = tmp%setindex(1, 2, 1.0)
+    hasindex = tmp%setindex(2, 1, 1.0)
+    hasindex = tmp%setindex(2, 2, 3.0)
 
-    ! tmp%m = 5
-    ! tmp%n = 7
-    ! tmp%nnz = 0
-    ! hasindex = tmp%setindex(1, 1, 11.0)
-    ! hasindex = tmp%setindex(5, 7, 99.0)
-    tmp = sprand(10000, 0.01)
-    call tmp%summary
-
-    do j = 1, tmp%n
-        do i = j+1, tmp%m
-            hasindex = tmp%getindex(i, j, v)
-            hasindex = tmp%getindex(j, i, w)
-
-            if (abs(v - w) > 1e-16) then
-                print '("Não é simétrica! ", e10.4)', abs(v-w)
-            end if
-        end do
-    end do
     A = tmp%to_csc()
-    call A%summary
+    call A%print
+
+    b = [1, 2]
+    call solve(A, b, x)
+    print *, x
     call tmp%deallocate
-    call A%deallocate
-    ! print *, tmp%val
-    ! print *, tmp%getindex(5, 7, v)
-    ! A = tmp%to_csc()
-    ! call A%print
-    ! call tmp%deallocate
-    !
-    ! B = [1, 1, 1, 1, 1, 1, 1]
-    ! call A%times(B, C)
-    ! print *, C
 end program EP2
---
+++ EP2/Módulos/CG.f95
@@ -0,0 +1,51 @@
+module CG
+    use class_SparseMatrixCSC
+    implicit none
+    private
+    public :: solve
+contains
+    subroutine solve(A, b, x)
+        type(SparseMatrixCSC), intent(in)  :: A
+        real,                  intent(in)  :: b(:)
+        real,                  intent(out) :: x(:)
+
+        integer :: n, i
+        real, allocatable :: r(:), p(:), Ap(:)
+        real :: eps, rsold, rsnew, alpha
+        eps = 1e-10
+
+        n = size(b)
+        allocate(r(n))
+        allocate(p(n))
+        allocate(Ap(n))
+
+        ! Inicializa x
+        x(:) = 0.0
+        
+        ! r = b - A*x
+        call A%times(x, r)
+        r = b - r
+
+        p = r
+        rsold = dot_product(r, r)
+
+        do i = 1, n
+            call A%times(p, Ap)
+            alpha = rsold/dot_product(p, Ap)
+            x = x + alpha*p
+            r = r - alpha*Ap
+
+            rsnew = dot_product(r, r)
+            if (sqrt(rsnew) < eps) then
+                return
+            end if
+
+            p = r + (rsnew/rsold)*p
+            rsold = rsnew
+        end do
+
+        deallocate(r)
+        deallocate(p)
+        deallocate(Ap)
+    end subroutine
+end module CG
