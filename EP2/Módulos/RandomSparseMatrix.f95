c29c042d3ad18dc0cdaeee80e5bae5d79b7d956e
Lucas Magno <lmagno94@gmail.com>
2015-11-01 23:37:36 -0200

    Começa o gerador de matrizes aleatórias

--- EP2/Módulos/RandomSparseMatrix.f95
+++ EP2/Módulos/RandomSparseMatrix.f95
@@ -5,52 +5,39 @@ module RandomSparseMatrix
     public :: sprand
 contains
     function sprand(n, tau) result(A)
-        integer, intent(in) :: m, n
+        integer, intent(in) :: n
         real,    intent(in) :: tau
 
-        type(SparseMatrixCOO) :: A, B
+        type(SparseMatrixCOO) :: A
 
-        integer :: i, j, k
-        integer :: s, nnz
+        integer :: i, j
+        integer :: hasindex
         real    :: v
-        ! Aproximação de quantos elementos não-nulos haverão
-        nnz = nint(2*tau*n**2)
 
-        call A%allocate(m = n, n = n, nnz = nnz)
+        call A%init(n, n)
 
-        k = 1
         do j = 1, n
             ! Parte triangular superior da matriz,
             ! onde os elementos já estão definidos devido à simetria.
             ! (Basta copiar o valor do elemento [j, i])
             do i = 1, j-1
                 if (A%getindex(j, i, v) == 1) then
-                    A%rowind(k) = i
-                    A%colind(k) = j
-                    A%val(k)    = v
-                    k = k + 1
+                    hasindex = A%setindex(i, j, v)
                 end if
             end do
 
             ! Coloca 1's na diagonal principal
-            A%rowind(k) = j
-            A%colind(k) = j
-            A%val(k)    = 1
-            k = k + 1
+            hasindex = A%setindex(j, j, 1.0)
 
             !
             do i = j+1, n
                 ! v ∈ [-1, 1]
                 v = 2*rand() - 1
                 if (abs(v) < tau) then
-                    A%rowind(k) = i
-                    A%colind(k) = j
-                    A%val(k)    = v
-                    k = k + 1
+                    hasindex = A%setindex(i, j, v)
                 end if
             end do
         end do
 
-        
     end function sprand
 end module RandomSparseMatrix
