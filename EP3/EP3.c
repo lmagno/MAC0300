0de2f6a40499528252b6fdb5bf61f29a5c09159e
Lucas Magno <lmagno94@gmail.com>
2015-11-22 20:34:18 -0200

    Plota somente se for um conjunto de dados

--- EP3/EP3.c
+++ EP3/EP3.c
@@ -14,7 +14,7 @@ int main(int argc, char** argv) {
     Matriz  A;
     QR      q;
     double *x;
-    char   *type, *input;
+    char   *type, *input, t;
     int i;
 
     // Determina onde começam os argumentos de verdade
@@ -25,14 +25,21 @@ int main(int argc, char** argv) {
     type  = argv[i];    // Tipo de entrada (sistema já montado ou dados)
     input = argv[i+1];  // Nome do arquivo de entrada
 
-    // Carrega o sistema (A e b) do arquivo de entrada
-    if (!strcmp("sys", type))
-        S = loadsys(input);
-    else if(!strcmp("data", type))
-        S = loaddata(input);
+    if(!strcmp("sys", type))
+        t = 's';
+    else if (!strcmp("data", type))
+        t = 'd';
     else {
         fprintf(stderr, "Tipo de entrada '%s' não reconhecido.\n", type);
         return 1;
+    }
+
+    // Carrega o sistema (A e b) do arquivo de entrada
+    switch(t) {
+        case 's':
+            S = loadsys(input);
+        case 'd':
+            S = loaddata(input);
     }
 
     A = S.A;
@@ -46,7 +53,10 @@ int main(int argc, char** argv) {
 
     // Salva o resultado para um arquivo de saída
     save(x, A.m, input);
-    plot(input, x);
+
+    // Plota se for um conjunto de dados com um polinômio ajustado
+    if (t == 'd')
+        plot(input, x);
 
     // Libera as memórias alocadas
     free(x);
